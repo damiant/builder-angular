58e4569c4e10403337930d24e1f33a89ce511b81
Damian Tarnawsky <damian@builder.io>
2024-12-20 12:02:48 -0800

    feat: set dev key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   builderPublicKey: "d6151a3b3c4f49dda6bb6c6541d060ee",
-  builderApiKey: "d6151a3b3c4f49dda6bb6c6541d060ee",
+  builderApiKey: "51990e4e54d84619b816ceea9e1d4fef",
 };
